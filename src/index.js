4c1245ecb3c38a920fa8800d4abb6e3716d261f9
michail-konon <greateyuri@gamil.com>
2023-02-06 22:07:45 +0300

    feat: add solution prototype

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const MORSE_TABLE = {
+    ' ':      ' ',
     '.-':     'a',
     '-...':   'b',
     '-.-.':   'c',
@@ -38,9 +39,22 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+	let arr = expr.split('');
+  let stepArr = [];
+  let length = arr.length / 10;
+  for(let i = 0; i < length; i++) {
+      stepArr.push(arr.splice(0, 10));
+    }
+  let zeroDel = stepArr.map((el) => Number(el.join('')));
+  let space = zeroDel.map((el) => isNaN(el) ? ' ' : el);
+  let toStr = space.map((el) => String(el).replace(/11/ig, '-').replace(/10/ig, '.'));
+  let almost = toStr.map((el) => MORSE_TABLE[el])
+  return  almost.join('')
 }
 
 module.exports = {
     decode
 }
+
+
+
